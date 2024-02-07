6b81f69ff6b7021c62c0f15090b6dced718de010
Izabelle <135071709+izabellelantz@users.noreply.github.com>
2024-02-07 16:49:23 -0600

    fixing and formatting

--- public/notes.js
+++ public/notes.js
@@ -8,6 +8,8 @@ button.addEventListener("click", () => {
     let trashIcon = document.createElement("i");
     let confirmIcon = document.createElement("i");
     let editIcon = document.createElement("i");
+    let titleInput = document.createElement("input");
+    let titleText = document.createElement("h3");
 
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
@@ -21,58 +23,55 @@ button.addEventListener("click", () => {
     notesContainer.appendChild(confirmIcon);
     notesContainer.appendChild(editIcon);
 
+
     // Delete note and trash icon when trash icon is clicked
     trashIcon.addEventListener("click", () => {
       inputBox.remove();
       trashIcon.remove();
       confirmIcon.remove();
       editIcon.remove();
+      titleText.remove();
     });
 
     confirmIcon.addEventListener("click", () => {
       // Make content not editable when confirmed
       inputBox.setAttribute("contenteditable", "false");
-      confirmation();
 
-      function confirmation() {
-        let confirmationBox = document.createElement("div");
-        let yesButton = document.createElement("button");
-        let noButton = document.createElement("button");
-        let titleInput = document.createElement("input");
-        let confirmIcon = document.createElement("i");
-        let titleText = document.createElement("h3");
-    
-        confirmIcon.className = "uil-check-square";
-        confirmationBox.className = "confirmation-box";
-        confirmationBox.textContent = "Do you want to save this note?";
-        yesButton.textContent = "Yes";
-        noButton.textContent = "No";
+      let confirmationBox = document.createElement("div");
+      let yesButton = document.createElement("button");
+      let noButton = document.createElement("button");
+      let confirmIcon = document.createElement("i");
+  
+      confirmIcon.className = "uil-check-square";
+      confirmationBox.className = "confirmation-box";
+      confirmationBox.textContent = "Do you want to save this note?";
+      yesButton.textContent = "Yes";
+      noButton.textContent = "No";
+      
+      notesContainer.appendChild(confirmationBox);
+      confirmationBox.append(yesButton);
+      confirmationBox.append(noButton);
+  
+      yesButton.addEventListener("click", () => {
+        confirmationBox.textContent = "What would you like to name your note?";
+        yesButton.remove();
+        noButton.remove();
         
-        notesContainer.appendChild(confirmationBox);
-        confirmationBox.append(yesButton);
-        confirmationBox.append(noButton);
-    
-        yesButton.addEventListener("click", () => {
-          confirmationBox.textContent = "What would you like to name your note?";
-          yesButton.remove();
-          noButton.remove();
-          
-          confirmationBox.appendChild(titleInput);
-          confirmationBox.appendChild(confirmIcon);
-    
-          confirmIcon.addEventListener("click", () => {
-            titleText.textContent = titleInput.value;
-            notesContainer.insertBefore(titleText, trashIcon);
-          
-            // Save to database ?
-          
-            confirmIcon.remove();
-            titleInput.remove();
-            confirmationBox.remove();
-          });
-          
+        confirmationBox.appendChild(titleInput);
+        confirmationBox.appendChild(confirmIcon);
+  
+        confirmIcon.addEventListener("click", () => {
+          titleText.textContent = titleInput.value;
+          notesContainer.insertBefore(titleText, trashIcon);
+        
+          // Save to database ?
+        
+          confirmIcon.remove();
+          titleInput.remove();
+          confirmationBox.remove();
         });
-      };
+        
+      });
     });
 
     editIcon.addEventListener("click", () => {
